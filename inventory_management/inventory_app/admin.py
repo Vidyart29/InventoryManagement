d3600353465abd1422cbe38d7e321a8cd2168895
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-02-08 19:36:59 +0530

    feat: Created models

--- inventory_management/inventory_app/admin.py
+++ inventory_management/inventory_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Admin, Product, ProductCategorie, orderHistory
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Admin)
+admin.site.register(Product)
+admin.site.register(ProductCategorie)
+admin.site.register(orderHistory)
