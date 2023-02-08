ae9338a558ec43a30166a22bee03b14c91c97dad
vidyarautela <vidya.rautela.28@gmail.com>
2023-02-08 19:17:20 +0530

    connected two html pages

--- inventory_management/inventory_management/urls.py
+++ inventory_management/inventory_management/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', include('inventory_app.urls'))
-]
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     # path('', include('inventory_app.urls'))
+# ]
 urlpatterns = [path("admin/", admin.site.urls), path("", include("inventory_app.urls"))]
