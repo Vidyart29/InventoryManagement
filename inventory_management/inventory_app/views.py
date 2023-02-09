41d23bbd28d05a5ffa157cc2d625b8e77db2691f
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-02-09 10:48:46 +0530

    merge incoming changes

--- inventory_management/inventory_app/views.py
+++ inventory_management/inventory_app/views.py
@@ -1,14 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 # from .forms import SignUpForm
 
 # Create your views here.
+@login_required(login_url="login")
 def index(request):
     return render(request, "index.html")
 
 def signupPage(request):
+    if request.method == "POST":
+        uname = request.POST.get("username")
+        email = request.POST.get("email")
+        pass1 = request.POST.get("password1")
+        pass2 = request.POST.get("password2")
+        if pass1 != pass2:
+            return HttpResponse("Your passwords do not match")
+        else:
+            my_user = User.objects.create_user(uname, email, pass1)
+            my_user.save()
+            return redirect("login")
+
+        # return HttpResponse("User is created successfully")
+        # print(fname, lname, email, pass1, pass2)
     return render(request, "signup.html")
 
 
 def loginPage(request):
+    if request.method == "POST":
+        username1 = request.POST.get("username")
+        pass1 = request.POST.get("password")
+        user = authenticate(request, username=username1, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return HttpResponse("Username or password is incorrect")
+
     return render(request, "login.html")
