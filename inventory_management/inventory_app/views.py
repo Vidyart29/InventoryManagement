cddf2eb3ad11711d29b4859de328dacc00a028f2
vidyarautela <vidya.rautela.28@gmail.com>
2023-02-09 16:16:51 +0530

    updating files

--- inventory_management/inventory_app/views.py
+++ inventory_management/inventory_app/views.py
@@ -10,40 +10,12 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, "index.html")
 
-
-def signupPage(request):
-    if request.method == "POST":
-        uname = request.POST.get("username")
-        email = request.POST.get("email")
-        pass1 = request.POST.get("password1")
-        pass2 = request.POST.get("password2")
-        if pass1 != pass2:
-            return HttpResponse("Your passwords do not match")
-        else:
-            my_user = User.objects.create_user(uname, email, pass1)
-            my_user.save()
-            return redirect("login")
-
-    return render(request, "signup.html")
-
-
-def loginPage(request):
-    if request.method == "POST":
-        username1 = request.POST.get("username")
-        pass1 = request.POST.get("password")
-        user = authenticate(request, username=username1, password=pass1)
-
-        if user is not None:
-            login(request, user)
-            print("HIeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
-            return redirect("index")
-
-        else:
-            return HttpResponse("Username or password is incorrect")
-
+def login(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        pass1 = request.POST.get("pass")
+        print(username, username=username, paswword=pass1 )
+        if User is not None:
+            login(request, User)
+            return redirect('home')
     return render(request, "login.html")
-
-
-def logoutPage(request):
-    logout(request)
-    return redirect("login")
