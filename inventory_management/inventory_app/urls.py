e4a129ad32193625d886aa1fe88bffa7cc7671a9
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-02-09 16:13:57 +0530

    Updated authentication logic

--- inventory_management/inventory_app/urls.py
+++ inventory_management/inventory_app/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path("home", views.index, name="index"),
     path("", views.index, name="login"),
-    path("signup", views.signupPage, name="signup"),
+    path("signup", views.Signup.post, name="signup"),
     path("login", views.loginPage, name="login"),
     path("logout", views.logoutPage, name="logout"),
+    path("products/", views.products, name="products"),
 ]
--- inventory_management/inventory_app/views.py
+++ inventory_management/inventory_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .models import *
+from django.views import View
 
 # from .forms import SignUpForm
 
@@ -11,20 +13,40 @@ def index(request):
     return render(request, "index.html")
 
 
-def signupPage(request):
-    if request.method == "POST":
-        uname = request.POST.get("username")
-        email = request.POST.get("email")
-        pass1 = request.POST.get("password1")
-        pass2 = request.POST.get("password2")
-        if pass1 != pass2:
-            return HttpResponse("Your passwords do not match")
-        else:
-            my_user = User.objects.create_user(uname, email, pass1)
-            my_user.save()
-            return redirect("login")
+class Signup(View):
+    def get(self, request):
+        return render(request, "signup.html")
 
-    return render(request, "signup.html")
+    def post(request):
+        if request.method == "POST":
+            uname = request.POST.get("username")
+            email = request.POST.get("email")
+            pass1 = request.POST.get("password1")
+            pass2 = request.POST.get("password2")
+            if pass1 != pass2:
+                return HttpResponse("Your passwords do not match")
+            else:
+                my_user = User.objects.create_user(uname, email, pass1)
+                my_user.save()
+                return redirect("login")
+
+        return render(request, "signup.html")
+
+    def validateCustomer(self, customer):
+        error_message = None
+        if not customer.username:
+            error_message = "Please Enter your Username !!"
+        elif len(customer.uname) < 3:
+            error_message = "Username must be 3 char long or more"
+        elif len(customer.password) < 5:
+            error_message = "Password must be 5 char long"
+        elif len(customer.email) < 5:
+            error_message = "Email must be 5 char long"
+        elif customer.isExists:
+            error_message = "Email Address Already Registered.."
+        # saving
+
+        return error_message
 
 
 def loginPage(request):
@@ -47,3 +69,8 @@ def loginPage(request):
 def logoutPage(request):
     logout(request)
     return redirect("login")
+
+
+def products(request):
+    products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
