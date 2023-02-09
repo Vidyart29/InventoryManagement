0129b2a0d47d346a3614b4ebff76abdd7b7b0146
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-02-09 09:56:52 +0530

    Added temp signup and login

--- inventory_management/inventory_app/urls.py
+++ inventory_management/inventory_app/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index)]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("signup", views.signupPage, name="signup"),
+    path("login", views.loginPage, name="login"),
+]
--- inventory_management/inventory_app/views.py
+++ inventory_management/inventory_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+# from .forms import SignUpForm
+
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+
+def signupPage(request):
+    return render(request, "signup.html")
+
+
+def loginPage(request):
+    return render(request, "login.html")
