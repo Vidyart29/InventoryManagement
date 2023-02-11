3eea96853df6edd391eeb5152723d0fb934665ce
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-02-12 03:07:48 +0530

    Added default value to bu_code in cart page; removed unnecessary import in views.py

--- inventory_management/inventory_app/views.py
+++ inventory_management/inventory_app/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import *
-from django.conf import settings
 from .email import sendMail
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
